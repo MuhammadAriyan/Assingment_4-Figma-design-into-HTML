dd71ac30fe213a97ed0c4b43a1ec8417c97127b5
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-10-23 21:56:44 +0500

    created home

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,16 @@ const page = () => {
           <li id='ContactLi'><Link href={''}>Contact</Link></li>
         </ul>
       </nav>
-      <section className=" flex items-center border-4 border-black " >
-          <Image className=' rounded-xl m-[40px]' width={1030} height={308} alt='hero' src={'/hero-section.png'}/>
+      <section className="  flex justify-around items-center h-[817px]" >
+        <div className=' p-2 w-[541px]'>
+        <h1 className='hHero text-5xl '>Hi, I am John,</h1>
+        <h1 className=' hHero text-5xl pb-4'>Creative Technologist</h1>
+        <p className=' text-lg pb-10 pt-4'>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+        <button className=' mt-4 mb-7 p-2 text-white bg-red-400 shadow-sm shadow-black'>Download Resume</button>
+        </div>
+        <div>
+          <Image className='' width={242} height={249} alt='hero' src={'/hero.png'}/>
+        </div>
       </section>
     </main>
   )
