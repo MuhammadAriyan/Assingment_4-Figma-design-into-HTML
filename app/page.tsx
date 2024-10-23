047368800417c644fdf8c40aab04b0055ae2b145
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-10-23 20:20:24 +0500

    added font

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import '@/app/globals.css'
+import Link from 'next/link'
 const page = () => {
   return (
     <main className='mainbody'>
-
+      <nav>
+        <ul>
+          <li id='WorksLi'><Link href={''}>Works</Link></li>
+          <li id='BlogLi'><Link href={''}>Blog</Link></li>
+          <li id='ContactLi'><Link href={''}>Contact</Link></li>
+        </ul>
+      </nav>
     </main>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },fontFamily: {
+        custom: ['Inter', 'sans-serif'], 
       },
     },
   },
