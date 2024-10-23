e2a431b297ade1058a4dfb0005130e4330482f4b
Muhammad Aryan <160228293+MuhammadAriyan@users.noreply.github.com>
2024-10-23 21:38:58 +0500

    updated files

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@/app/globals.css'
 import Link from 'next/link'
+import Image from 'next/image'
 const page = () => {
   return (
     <main className='mainbody'>
@@ -11,6 +12,9 @@ const page = () => {
           <li id='ContactLi'><Link href={''}>Contact</Link></li>
         </ul>
       </nav>
+      <section className=" flex items-center border-4 border-black " >
+          <Image className=' rounded-xl m-[40px]' width={1030} height={308} alt='hero' src={'/hero-section.png'}/>
+      </section>
     </main>
   )
 }
